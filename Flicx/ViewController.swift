a85c3c0772eedf7b6886ae21da4e81e2f62598b3
ssaladd <58093617+ssaladd@users.noreply.github.com>
2022-01-30 22:58:55 -0500

    Initial Commit

---
+++ Flicx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flicx
+//
+//  Created by Saad Naji on 1/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
