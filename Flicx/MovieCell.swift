f9904e3938bbc50e54f566694ae7f967e1d29bd5
ssaladd <58093617+ssaladd@users.noreply.github.com>
2022-02-01 00:08:56 -0500

    -

---
+++ Flicx/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  Flicx
+//
+//  Created by Saad Naji on 1/31/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flicx' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flicx
+
+  target 'FlicxTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FlicxUITests' do
+    # Pods for testing
+  end
+
+end
